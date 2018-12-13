6d7bbcc423f5fbef3f37470e0e7adf60b2d68101
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2018-12-13 00:10:04 +0000

    Add bindings for 32 bit Linux

--- rdkafka-sys/src/bindings/mod.rs
+++ rdkafka-sys/src/bindings/mod.rs
@@ -4,6 +4,13 @@
 #[allow(non_snake_case)]
 mod linux_64;
 
+#[cfg(all(target_os = "linux", target_pointer_width = "32"))]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+mod linux_32;
+
+
 #[cfg(all(target_os = "linux", target_pointer_width = "64"))]
 pub use self::linux_64::*;
 
@@ -16,6 +23,7 @@ mod macos_64;
 #[cfg(all(target_os = "macos", target_pointer_width = "64"))]
 pub use self::macos_64::*;
 
-#[cfg(not(any(all(target_os = "linux", target_pointer_width = "64"),
+#[cfg(not(any(all(target_os = "linux", target_pointer_width = "32"),
+              all(target_os = "linux", target_pointer_width = "64"),
               all(target_os = "macos", target_pointer_width = "64"))))]
 compile_error!("Your platform is not yet supported. Build your rdkafka-sys bindings manually");
